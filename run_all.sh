37fdd7a4504dd1fb859fec8f84caa6ddf220024c
AntoineNGUYEN31 <nguyen.ensma@gmail.com>
2021-04-22 10:29:45 +0200

    add reloader

--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,10 @@
-docker run -d --network host -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml \
-    prom/prometheus
-docker run -d -p 3000:3000 --network host grafana/grafana
+docker run -d --network host \
+-v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml \
+prom/prometheus
+
+docker run -d -p 3000:3000 --network host \
+ -v $PWD/provisioning:/etc/grafana/provisioning \
+  -v $PWD/dashboards:/etc/dashboards \
+ grafana/grafana
+
+# -v $PWD/grafana.ini:/etc/grafana/grafana.ini \
