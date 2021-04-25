40ae6cf74583f0a78e8062591e4982aa059d57ab
AntoineNGUYEN31 <nguyen.ensma@gmail.com>
2021-04-25 21:09:53 +0200

    finish first version

---
+++ install.sh
@@ -0,0 +1,11 @@
+docker run -d --network host \
+-v $PWD/prometheus-conf/prometheus.yml:/etc/prometheus/prometheus.yml \
+prom/prometheus
+
+docker run -d -p 3000:3000 --network host \
+-v $PWD/grafana-conf:/etc/grafana/provisioning \
+grafana/grafana
+
+docker run -d --network host \
+-v $1:/storage \
+etangdesapplis/pv-exporter
---
+++ kubernetes-csi-nfs/README.md
@@ -0,0 +1,6 @@
+# TL;DR;
+```
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner-4.0.8.tgz \
+ --set nfs.server=192.168.1.89 \
+ --set nfs.path=/nfs
+```
